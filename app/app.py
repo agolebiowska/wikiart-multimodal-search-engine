66fdd4d88ee6b20b14e97b3a09b1f112d541d23a
gagata <gagata@google.com>
2024-01-07 15:23:30 +0000

    update interface, add filtering

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import gradio as gr
 from PIL import Image as PILImage
 
 from google.cloud import aiplatform, storage, bigquery
-from google.cloud.aiplatform import MatchingEngineIndex, MatchingEngineIndexEndpoint
+from google.cloud.aiplatform import MatchingEngineIndex, MatchingEngineIndexEndpoint, matching_engine
 from vertexai.preview.vision_models import MultiModalEmbeddingModel, Image
 
 from dotenv import load_dotenv
@@ -31,32 +31,64 @@ index = None
 index_endpoint = None
 
 
-def get_path(name):
-    return f"{DATA_BUCKET}/{ALL_PREFIX}/{name}.jpg"
+def get_path(name, category):
+    return f"{ALL_PREFIX}/{category}/{name}.jpg"
 
 
-def get_url(name):
-    return f"https://storage.cloud.google.com/{get_path(name)}"
+def get_url(name, category):
+    return f"https://storage.cloud.google.com/{DATA_BUCKET}/{get_path(name, category)}"
+
+
+def extract_author_title(image_id):
+    parts = image_id.split('_')
+    author = parts[0].replace('-', ' ')
+    title = '_'.join(parts[1:])
+    return author, title
+
+
+def format_genre(genre):
+    cleaned_string = genre.strip("[]").replace("'", "")
+    cleaned_string = cleaned_string.split(",")[0]
+    result = cleaned_string.lower().replace(" ", "-")
+    return result
+
+
+def format_folder(genre):
+    cleaned_string = genre.replace(" ", "-")
+    result = cleaned_string.lower()
+    return result
+
+
+def download_from_gcs(image_uri, data_bucket):
+    try:
+        client = storage.Client()
+        bucket = client.bucket(data_bucket)
+        blob = bucket.blob(image_uri)
+        blob.download_to_filename(image_uri)
+    except Exception as ex:
+        logging.error(f"Error downloading {image_uri} from GCS: {ex}")
 
 
 def get_image_metadata(image_id):
     try:
-        filename = get_path(image_id)
+        author, title = extract_author_title(image_id)
         client = bigquery.Client(project=PROJECT_ID)
         query = f"""
         SELECT artist, genre, description
         FROM `{PROJECT_ID}.{DATASET}.{TABLE}`
-        WHERE filename = @filename
+        WHERE artist = @author AND description = @title
         """
         job_config = bigquery.QueryJobConfig(
-            query_parameters=[bigquery.ScalarQueryParameter("filename", "STRING", filename)]
+            query_parameters=[
+                bigquery.ScalarQueryParameter("author", "STRING", author),
+                bigquery.ScalarQueryParameter("title", "STRING", title)]
         )
 
         query_job = client.query(query, job_config=job_config)
         result = query_job.result(max_results=1)
 
         for row in result:
-            return artist, description, genre
+            return row.artist, row.description, format_genre(row.genre)
 
         return "", "", ""
     except Exception as ex:
@@ -64,28 +96,34 @@ def get_image_metadata(image_id):
         return "", "", ""
 
 
-def get_label(image_id):
-    artist, description, genre = get_image_metadata(image_id)
-    return f"{artist} - {description} - {genre}"
+def get_label(artist, description, genre):
+    return f"{artist.title()} - {description.replace('-', ' ').title()} - {genre.replace('-', ' ').title()}"
 
 
-def get_matches(query_emb):
+def get_matches(query_emb, num_results, filter):
     result = index_endpoint.match(
         DEPLOYED_INDEX_ID,
         queries=[query_emb],
-        num_neighbors=20
+        num_neighbors=int(num_results),
+        filter=[matching_engine.matching_engine_index_endpoint.Namespace(
+            "category", filter, [])]
     )
 
     matches = []
     for match in result[0]:
+        artist, description, genre = get_image_metadata(match.id)
+        
         matches.append(
-            (get_path(match.id), get_label(match.id))
+            (get_url(match.id, genre), get_label(artist, description, genre))
         )
 
     return matches
 
 
-def image_query(image_data):
+def image_query(image_data, num_results, genres_filter):
+    if image_data is None:
+        raise gr.Error("Image cannot be empty")
+    
     try:
         image = PILImage.fromarray(np.uint8(image_data))
         image_path = "image.jpg"
@@ -93,20 +131,27 @@ def image_query(image_data):
         query_image = Image.load_from_file(image_path)
         query_emb = multimodalembedding.get_embeddings(
             image=query_image).image_embedding
+
+        filter = [format_folder(genre) for genre in genres_filter]
             
-        return get_matches(query_emb)
+        return get_matches(query_emb, num_results, filter)
 
     except Exception as ex:
         logger.error(f"Error: {ex}")
         return []
 
 
-def text_query(text):
+def text_query(text, num_results, genres_filter):
+    if len(text) <= 0:
+        raise gr.Error("Query cannot be empty")
+
     try:
         query_emb = multimodalembedding.get_embeddings(
             contextual_text=text).text_embedding
 
-        return get_matches(query_emb)
+        filter = [format_folder(genre) for genre in genres_filter]
+
+        return get_matches(query_emb, num_results, filter)
 
     except Exception as ex:
         logger.error(f"Error: {ex}")
@@ -114,27 +159,83 @@ def text_query(text):
 
 
 def create_ui():
-    with gr.Blocks() as iface:
-        gr.Markdown("Wikiart Search engine")
+    with gr.Blocks(theme=gr.themes.Default(primary_hue=gr.themes.colors.emerald,
+                                           secondary_hue=gr.themes.colors.emerald)) as iface:
+
+        gr.Markdown("# Multimodal Search Engine for Art")
+
+        genres = [
+            "Expressionism",
+            "Abstract expressionism",
+            "Action painting",
+            "Analytical cubism",
+            "Art Nouveau modern",
+            "Baroque",
+            "Color field painting",
+            "Realism",
+            "Contemporary realism",
+            "New Realism",
+            "Cubism",
+            "Synthetic cubism",
+            "Early Renaissance",
+            "High Renaissance",
+            "Mannerism late Renaissance",
+            "Northern Renaissance",
+            "Fauvism",
+            "Impressionism",
+            "Post Impressionism",
+            "Minimalism",
+            "Naive art Primitivism",
+            "Pointillism",
+            "Pop art",
+            "Rococo",
+            "Romanticism",
+            "Symbolism",
+            "Ukiyo-e"
+        ]
 
         with gr.Tab("Image-to-image search"):
             image = gr.Image()
-            find_by_image_btn = gr.Button("Get similar images")
+            genres_filter = gr.CheckboxGroup(genres,
+                                            label="Genres",
+                                            info="Optional. Select genres to filter.")
+            
+            with gr.Row():
+                num_results = gr.Dropdown([5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 
+                                        label="Number of results",
+                                        value=20,
+                                        info="How many results to show.")
+                clear = gr.ClearButton(components=[image, genres_filter])
+                find_by_image_btn = gr.Button("Get images", variant="primary", icon="icon_search.svg")
+
+            images = gr.Gallery(label="Images",
+                                show_label=True,
+                                columns=[5],
+                                object_fit="cover")
+
+            find_by_image_btn.click(image_query, inputs=[image, num_results, genres_filter], outputs=[images])
+
 
         with gr.Tab("Text-to-image search"):
-            text = gr.Text(label="Text query")
-            find_by_text_btn = gr.Button("Get images with similar semantic value")
+            text = gr.Textbox(label="Text query")
+            genres_filter = gr.CheckboxGroup(genres,
+                                            label="Genres",
+                                            info="Optional. Select genres to filter.")
+            
+            with gr.Row():
+                num_results = gr.Dropdown([5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 
+                                        label="Number of results",
+                                        value=20,
+                                        info="How many results to show.")
+                clear = gr.ClearButton(components=[text, genres_filter])
+                find_by_text_btn = gr.Button("Get images", variant="primary", icon="icon_search.svg")
 
-        images = gr.Gallery(label="Images",
-                            show_label=True,
-                            elem_id="gallery",
-                            columns=[4],
-                            object_fit="contain",
-                            scale=0)
-
-        find_by_image_btn.click(image_query, inputs=[image], outputs=[images])
-        find_by_text_btn.click(text_query, inputs=[text], outputs=[images])
-        # gr.Examples([], [])
+            images = gr.Gallery(label="Images",
+                                show_label=True,
+                                columns=[5],
+                                object_fit="cover")
+        
+            find_by_text_btn.click(text_query, inputs=[text, num_results, genres_filter], outputs=[images])
 
     return iface
 
@@ -154,6 +255,6 @@ if __name__ == '__main__':
         logger.error(f"Could not get Vertex AI Endpoint: {ex}")
 
     ui = create_ui()
-    ui.queue().launch(inline=False,
-                      server_name="0.0.0.0",
-                      server_port=7860)
+    ui.launch(inline=False,
+              server_name="0.0.0.0",
+              server_port=7860)
